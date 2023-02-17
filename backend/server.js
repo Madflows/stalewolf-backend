758091a012c4c6fe5b52e80e6b28c36679592234
Folarin Lawal <lawalfolarin6@gmail.com>
2023-02-17 01:00:22 +0100

    added CORS and home route

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import colors from "colors";
+import cors from "cors";
 import userRoutes from "./routes/userRoutes.js";
 import cookieParser from "cookie-parser";
 import { connectDB } from "./config/db.js";
@@ -10,6 +11,7 @@ connectDB();
 const PORT = 5000;
 
 const app = express();
+app.use(cors())
 app.use(express.json());
 app.use(cookieParser())
 app.use(
@@ -19,6 +21,18 @@ app.use(
 );
 
 app.use("/api/music", userRoutes);
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Club request api",
+    routes: {
+      "list-all-songs": "GET /api/music",
+      "make-a-request (WIP)": "POST /api/requests",
+      "list-all-requests (WIP)": "GET /api/requests",
+      "delete-request (WIP)": "DELETE /api/requests/:id",
+    }
+  })
+})
 
 console.log("wahala");
 
